8f06539e683c71e01ff08b12770a684884c9ad3a
FredrichT <86357919+FredrichT@users.noreply.github.com>
2024-11-26 22:49:39 +0100

    gru

--- .history/config_20241123201337.py
+++
@@ -1,6 +0,0 @@
-learning_rate = 1e-4
-nepochs = 10
-batch_size = 64
-max_len = 8
-hidden_size = 8
-num_layers = 1
--- .history/config_20241123221735.py
+++
@@ -1,7 +0,0 @@
-# Define the hyperparameters
-learning_rate = 1e-3
-nepochs = 2        # Very few epochs
-batch_size = 128   # Larger batch size for faster processing
-max_len = 2        # Short sequences
-hidden_size = 2    # Tiny hidden size
-num_layers = 1     # Single layer
